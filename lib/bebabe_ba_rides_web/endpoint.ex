beb363b81856de43cc29a566fae3432137d87320
Isaacrodgers <162332330+Isaacrodgers@users.noreply.github.com>
2026-04-21 10:00:28 +0300

    Create endpoint.ex

---
+++ lib/bebabe_ba_rides_web/endpoint.ex
@@ -0,0 +1,32 @@
+defmodule BEBABEBA_RIDESWeb.Endpoint do
+  use Phoenix.Endpoint, otp_app: :bebabe_ba_rides
+
+  socket "/socket", BEBABEBA_RIDESWeb.UserSocket
+
+  # Serve at "/" the static files from "priv/static"
+  plug Plug.Static,
+    at: "/",
+    from: :bebabe_ba_rides,
+    gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  # Code reloading can be explicitly enabled under the
+  #  [:development] environment.
+  if code_reloading? do
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Jason
+  plug Plug.MethodOverride
+  plug Plug.Head
+  plug Plug.Session, 
+    store: :cookie,
+    key: "_bebabe_ba_rides_key",
+    signing_salt: "randomsalt"
+  plug BEBABEBA_RIDESWeb.Router
+end
